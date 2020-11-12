51542f3508becc646cd04f66a4a8ad12dae7f17b
cindygonsales.25@gmail.com <>
2020-11-11 23:00:49 -0600

    Subiendo la Tarea de Abstraccion

---
+++ src/paises/USA.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paises;
+
+/**
+ *
+ * @author CINDY GONZALEZ
+ */
+public class USA extends pais{
+    
+    public String getpais(){
+  return "Estados Unidos De Norte America"; 
+   }
+public String getpresidente(){
+   return "Donal Troum";
+}
+    
+}
